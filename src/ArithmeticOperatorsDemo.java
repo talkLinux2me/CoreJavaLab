641b3e323ecf96c2a9af42b97969917b9f5724ed
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-06-27 10:31:32 -0400

    Started artithmetoc operators

---
+++ src/ArithmeticOperatorsDemo.java
@@ -0,0 +1,26 @@
+public class ArithmeticOperatorsDemo {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int x, y = 10, z = 5;
+        x = y + z;
+        System.out.println("+ operator resulted in " + x);
+        x = y - z;
+        System.out.println("- operator resulted in " + x);
+        x = y * z;
+        System.out.println("* operator resulted in " + x);
+        x = y / z;
+        System.out.println("/ operator resulted in " + x);
+        x = y % z;
+        System.out.println("% operator resulted in " + x);
+        x = y++;
+        System.out.println("Postfix ++ operator resulted in " + x);
+        x = ++z;
+        System.out.println("Prefix ++ operator resulted in " + x);
+        x = -y;
+        System.out.println("Unary operator resulted in " + x);
+
+
+
+
+
+}
