51a535c307e94dd4e4fad10fdea479527b581a6b
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-06-27 10:32:57 -0400

    finishing special cases

--- src/ArithmeticOperatorsDemo.java
+++ src/ArithmeticOperatorsDemo.java
@@ -28,6 +28,15 @@ public class ArithmeticOperatorsDemo {
         System.out.println("tooBig becomes " + tooBig);
         System.out.println("tooSmall becomes " + tooSmall);
 
+        System.out.println(4.0 / 0.0); // Prints: Infinity
+        System.out.println(-4.0 / 0.0); // Prints: -Infinity
+        System.out.println(0.0 / 0.0); // Prints: NaN
+        double d1 = 12 / 8; // result: 1 by integer division. The value of d1 becomes 1.0.
+        double d2 = 12.0F / 8; // result: 1.5
+        System.out.println("d1 is " + d1);
+        System.out.println("d2 is " + d2);
+    }
+}
 
 
 }
