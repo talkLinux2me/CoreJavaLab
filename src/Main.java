a18101976331e0dd8682299267bd34c5d57bf36d
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-06-27 09:41:16 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int g = 5;
+        int j = 8;
+
+        {
+
+
+            System.out.println("Hello world!");
+        }
+    }}
